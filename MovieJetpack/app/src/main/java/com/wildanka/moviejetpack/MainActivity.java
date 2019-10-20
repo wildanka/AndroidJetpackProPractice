f37d7838b7d5e3ff2bc6efe369200c55b62dcccc
WildanKurniadi <danprogramming10@gmail.com>
2019-10-20 13:50:29 +0700

    #MovieJetpck

---
+++ MovieJetpack/app/src/main/java/com/wildanka/moviejetpack/MainActivity.java
@@ -0,0 +1,25 @@
+package com.wildanka.moviejetpack;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextMessage;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextMessage = findViewById(R.id.message);
+    }
+
+}
