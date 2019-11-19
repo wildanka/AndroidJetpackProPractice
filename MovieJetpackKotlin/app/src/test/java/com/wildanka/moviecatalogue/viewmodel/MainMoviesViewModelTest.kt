f75f05ae6ae33be82577c17d3fc601576c94be1a
WildanKurniadi <danprogramming10@gmail.com>
2019-11-19 11:06:52 +0700

    Submission2 #19 UnitTesting ViewModelTest

--- MovieJetpackKotlin/app/src/test/java/com/wildanka/moviecatalogue/viewmodel/MainMoviesViewModelTest.kt
+++ MovieJetpackKotlin/app/src/test/java/com/wildanka/moviecatalogue/viewmodel/MainMoviesViewModelTest.kt
@@ -1,14 +1,18 @@
 package com.wildanka.moviecatalogue.viewmodel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import com.wildanka.moviecatalogue.MoviesRepository
-import com.wildanka.moviecatalogue.util.getOrAwaitValue
-import org.junit.Assert
+import com.wildanka.moviecatalogue.model.entity.MovieData
+import com.wildanka.moviecatalogue.model.entity.TVShowData
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mock
 import org.mockito.Mockito
+import org.mockito.Mockito.*
+import java.math.BigInteger
 
 class MainMoviesViewModelTest {
     @get:Rule
@@ -28,15 +32,65 @@ class MainMoviesViewModelTest {
 
     @Test
     fun getMovieList() {
-        //test mainViewModel.getMovieList is it return movie list (more than 10)
-        val expectedSize = 10
-        Assert.assertTrue("MovieList Data", mainViewModel.getMovieList()?.getOrAwaitValue()?.size ?: 0 > expectedSize)
+        val mainViewModel = mock(MainMoviesViewModel::class.java)
+        val dummyMovie = MovieData(
+            "475557",
+            BigInteger("5589"),
+            "Joker",
+            "2019-10-02",
+            8.4,
+            "During the 1980s, a failed stand-up comedian is driven insane and turns to a life of crime and chaos in Gotham City while becoming an infamous psychopathic crime figure.",
+            "/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg",
+            "en",
+            "422.288",
+            false
+        )
+        val movieDummyLiveData: MutableLiveData<MutableList<MovieData>> =
+            MutableLiveData() //construct dummyMutableLiveData
+
+        //buat data list dummy
+        val dummyMovieList: MutableList<MovieData> = mutableListOf()
+        for (i in 0..15) dummyMovieList.add(dummyMovie)
+        movieDummyLiveData.value = dummyMovieList
+
+        `when`(mainViewModel.getMovieList()).thenReturn(movieDummyLiveData)
+
+        //verify onChanged pada livedata
+        val observer: Observer<MutableList<MovieData>> = mock(Observer::class.java) as Observer<MutableList<MovieData>>
+        mainViewModel.getMovieList()?.observeForever(observer)
+        verify(observer).onChanged(dummyMovieList)
     }
 
     @Test
     fun getTVShowList() {
-        //test mainViewModel.getTVShowList is it return tv show list (more than 10)
-        val expectedSize = 10
-        Assert.assertTrue("TVShowList Data", mainViewModel.getTVShowList()?.getOrAwaitValue()?.size ?: 0 > expectedSize)
+        val mainViewModel = mock(MainMoviesViewModel::class.java)
+        val dummyTVShow = TVShowData(
+            "71712",
+            BigInteger("432"),
+            "The Good Doctor",
+            "The Good Doctor",
+            "2017-09-25",
+            7.7,
+            "A young surgeon with Savant syndrome is recruited into the surgical unit of a prestigious hospital. The question will arise: can a person who doesn't have the ability to relate to people actually save their lives?",
+            "/53P8oHo9cfOsgb1cLxBi4pFY0ja.jpg",
+            "en",
+            "80.784"
+        )
+        val tvShowDummyLiveData: MutableLiveData<MutableList<TVShowData>> =
+            MutableLiveData() //construct dummyMutableLiveData
+
+        //buat data list dummy
+        val dummyTVShowList: MutableList<TVShowData> = mutableListOf()
+        for (i in 0..15) dummyTVShowList.add(dummyTVShow)
+        tvShowDummyLiveData.value = dummyTVShowList
+
+        //return dummy data
+        `when`(mainViewModel.getTVShowList()).thenReturn(tvShowDummyLiveData)
+
+        //verify onChanged pada livedata
+        val observer: Observer<MutableList<TVShowData>> = mock(Observer::class.java) as Observer<MutableList<TVShowData>>
+        mainViewModel.getTVShowList()?.observeForever(observer)
+        verify(observer).onChanged(dummyTVShowList)
+
     }
 }
