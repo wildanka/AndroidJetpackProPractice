23c42230bf9af54af207ece904547b77d1475b68
WildanKurniadi <danprogramming10@gmail.com>
2019-12-02 23:10:51 +0700

    Submission3 #2 - FavoritesFragment

---
+++ MovieJetpackKotlin/app/src/main/java/com/wildanka/moviecatalogue/view/FavoritesFragment.kt
@@ -0,0 +1,34 @@
+package com.wildanka.moviecatalogue.view
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+import com.wildanka.moviecatalogue.R
+import com.wildanka.moviecatalogue.view.adapter.MovieFragmentPagerAdapter
+
+class FavoritesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val rootView = inflater.inflate(R.layout.fragment_favorites, container, false)
+        val adapter = MovieFragmentPagerAdapter(activity!!.supportFragmentManager)
+
+        val vpMovie = rootView.findViewById<ViewPager>(R.id.vp_movie)
+        val tlMenu = rootView.findViewById<TabLayout>(R.id.tl_menu)
+        adapter.addFragment(MovieFragment(), "Movie")
+        adapter.addFragment(TVShowFragment(), "TV Show")
+        vpMovie.adapter = adapter
+        tlMenu.setupWithViewPager(vpMovie)
+        return rootView
+    }
+
+
+}
