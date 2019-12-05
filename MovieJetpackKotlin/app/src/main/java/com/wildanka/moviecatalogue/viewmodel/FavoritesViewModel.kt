7cd68acf091adec3446baf5bca06303884225739
WildanKurniadi <danprogramming10@gmail.com>
2019-12-05 18:05:32 +0700

    Submission3 #05.1 - create FavoritesViewModel and ViewModelFactory

--- MovieJetpackKotlin/app/src/main/java/com/wildanka/moviecatalogue/viewmodel/FavoritesViewModel.kt
+++ MovieJetpackKotlin/app/src/main/java/com/wildanka/moviecatalogue/viewmodel/FavoritesViewModel.kt
@@ -1,2 +1,20 @@
 package com.wildanka.moviecatalogue.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.wildanka.moviecatalogue.model.db.FavoritesRepository
+import com.wildanka.moviecatalogue.model.entity.MovieData
+
+class FavoritesViewModel(application: Application) : ViewModel(){
+    private var favoritesRepository: FavoritesRepository? = null
+
+    init {
+        favoritesRepository = FavoritesRepository(application)
+    }
+
+    fun getAllFavoritesMovies() : LiveData<List<MovieData>>? {
+        return favoritesRepository?.getAllFavoriteMovies()
+    }
+}
