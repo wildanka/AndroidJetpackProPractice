cc584759827e436b4a8e0f4272b76a88bb198615
Wildan Kurniadi <danprogramming10@gmail.com>
2021-05-03 06:31:01 +0700

    Submission 3 - #23 use childFragmentManager for ViewPagerAdapter inside a Fragment

--- MovieJetpackKotlin/app/src/main/java/com/wildanka/moviecatalogue/favorite/view/FavoritesFragment.kt
+++ MovieJetpackKotlin/app/src/main/java/com/wildanka/moviecatalogue/favorite/view/FavoritesFragment.kt
@@ -5,31 +5,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.viewpager.widget.ViewPager
-import com.google.android.material.tabs.TabLayout
 import com.wildanka.moviecatalogue.R
+import com.wildanka.moviecatalogue.databinding.FragmentFavoritesBinding
 import com.wildanka.moviecatalogue.remote.view.adapter.MovieFragmentPagerAdapter
 
 class FavoritesFragment : Fragment() {
-
+    private lateinit var binding: FragmentFavoritesBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val rootView = inflater.inflate(R.layout.fragment_favorites, container, false)
-        val adapter =
-            MovieFragmentPagerAdapter(activity!!.supportFragmentManager)
-
-        val vpMovie = rootView.findViewById<ViewPager>(R.id.vp_movie)
-        val tlMenu = rootView.findViewById<TabLayout>(R.id.tl_menu)
-        adapter.addFragment(FavoritesMovieFragment(), "Movies")
-        adapter.addFragment(FavoritesTVShowFragment(), "TV Shows")
-        vpMovie.adapter = adapter
-        tlMenu.setupWithViewPager(vpMovie)
-        return rootView
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_favorites, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val adapter =
+            MovieFragmentPagerAdapter(childFragmentManager)
 
+        adapter.addFragment(FavoritesMovieFragment(), "Movies")
+        adapter.addFragment(FavoritesTVShowFragment(), "TV Shows")
+        binding.vpMovie.adapter = adapter
+        binding.tlMenu.setupWithViewPager(binding.vpMovie)
+    }
 }
