2d41b865aa05fab5ae1d8b0d384233a171a11ef5
Wildan Kurniadi <wildan.kurniadi@klikdokter.com>
2021-05-24 03:06:37 +0700

    Submission 3 - #52 SearchViewModelTest.kt Done[now all search related stuff has been tested] (next TODO: remote `viewmodels for search feature` test)

---
+++ MovieJetpackKotlin/app/src/test/java/com/wildanka/moviecatalogue/presentation/ui/search/SearchViewModelTest.kt
@@ -0,0 +1,71 @@
+package com.wildanka.moviecatalogue.presentation.ui.search
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.wildanka.moviecatalogue.data.MoviesRepository
+import com.wildanka.moviecatalogue.data.datasource.local.entity.MovieData
+import com.wildanka.moviecatalogue.data.datasource.local.entity.TVShowData
+import com.wildanka.moviecatalogue.domain.entity.MovieCredits
+import com.wildanka.moviecatalogue.presentation.ui.movies.MainMoviesViewModel
+import com.wildanka.moviecatalogue.util.DataDummy
+import org.junit.Assert
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.mockito.Mock
+import org.mockito.Mockito
+
+class SearchViewModelTest {
+    @get:Rule
+    val instantTaskExecutorRule: InstantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Mock
+    private lateinit var searchViewModel: SearchViewModel
+
+    @Mock
+    private lateinit var moviesRepository: MoviesRepository
+
+    @Before
+    fun setUp() {
+        moviesRepository = Mockito.mock(MoviesRepository::class.java)
+        searchViewModel =
+            SearchViewModel(moviesRepository)
+    }
+
+    @Test
+    fun getSearchedMovies() {
+        val movieSearchQuery = "taken"
+        val dummyMovieSearchResult = DataDummy.generateRemoteDummyMovies()
+        val movieSearchResult = MutableLiveData<MutableList<MovieData>>()
+        movieSearchResult.value = dummyMovieSearchResult
+
+        Mockito.`when`(moviesRepository.searchMovies(movieSearchQuery))
+            .thenReturn(movieSearchResult)
+        val searchedMoviesResult = searchViewModel.getSearchedMovies(movieSearchQuery)?.value
+
+        Mockito.verify(moviesRepository).searchMovies(movieSearchQuery)
+        assertNotNull(searchedMoviesResult)
+        assertEquals(dummyMovieSearchResult.size, searchedMoviesResult?.size)
+
+    }
+
+    @Test
+    fun getSearchedTVShows() {
+        val tvShowSearchQuery = "winter"
+        val dummyTVShowSearchResult = DataDummy.generateRemoteDummyTVShow()
+        val tvShowSearchResult = MutableLiveData<MutableList<TVShowData>>()
+        tvShowSearchResult.value = dummyTVShowSearchResult
+
+        Mockito.`when`(moviesRepository.searchTVShows(tvShowSearchQuery))
+            .thenReturn(tvShowSearchResult)
+        val searchedTVShowResult = searchViewModel.getSearchedTVShows(tvShowSearchQuery)?.value
+
+        Mockito.verify(moviesRepository).searchTVShows(tvShowSearchQuery)
+        assertNotNull(searchedTVShowResult)
+        assertEquals(dummyTVShowSearchResult.size, searchedTVShowResult?.size)
+
+    }
+}
