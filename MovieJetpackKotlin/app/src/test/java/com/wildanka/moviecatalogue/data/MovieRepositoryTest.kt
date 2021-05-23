95dd5eedb4141a2e4f2b4ecb62b42da9a6ba1800
Wildan Kurniadi <wildan.kurniadi@klikdokter.com>
2021-05-24 02:54:01 +0700

    Submission 3 - #51 Remote MovieRepositoryTest.kt Done[search feature on repository has been tested] (next TODO: remote `viewmodels for search feature` test)

--- MovieJetpackKotlin/app/src/test/java/com/wildanka/moviecatalogue/data/MovieRepositoryTest.kt
+++ MovieJetpackKotlin/app/src/test/java/com/wildanka/moviecatalogue/data/MovieRepositoryTest.kt
@@ -1,17 +1,15 @@
 package com.wildanka.moviecatalogue.data
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.MutableLiveData
 import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.doAnswer
 import com.nhaarman.mockitokotlin2.eq
 import com.nhaarman.mockitokotlin2.verify
-import com.wildanka.moviecatalogue.data.datasource.local.entity.TVShowDetail
 import com.wildanka.moviecatalogue.data.datasource.remote.RemoteDataSource
 import com.wildanka.moviecatalogue.util.DataDummy
 import com.wildanka.moviecatalogue.util.LiveDataTestUtil
-import junit.framework.Assert.assertEquals
-import junit.framework.Assert.assertNotNull
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito
@@ -27,6 +25,8 @@ class MovieRepositoryTest {
     private val movieId = "460465"
     private val tvShowResponses = DataDummy.generateRemoteDummyTVShow()
     private val tvShowId = "88396"
+    private val movieSearchQuery = "taken"
+    private val tvShowSearchQuery = "winter"
 
     private val detailMovie = DataDummy.generateRemoteDummyMovieDetail(movieId)
     private val detailTVShow = DataDummy.generateRemoteDummyTVShowDetail(tvShowId)
@@ -34,9 +34,11 @@ class MovieRepositoryTest {
     private val tvShowCredits = DataDummy.generateRemoteDummyTVShowCredits(tvShowId)
 
     @Test
-    fun getAllMovies(){
+    fun getAllMovies() {
         doAnswer { invocationOnMock ->
-            (invocationOnMock.arguments[0] as RemoteDataSource.LoadMoviesCallback).onAllMovieReceived(movieResponses)
+            (invocationOnMock.arguments[0] as RemoteDataSource.LoadMoviesCallback).onAllMovieReceived(
+                movieResponses
+            )
         }.`when`(remote).fetchMovieData(any())
         val movies = LiveDataTestUtil.getValue(fakeMovieRepository.fetchMovieData())
         verify(remote).fetchMovieData(any())
@@ -46,9 +48,11 @@ class MovieRepositoryTest {
     }
 
     @Test
-    fun getAllTVShow(){
+    fun getAllTVShow() {
         doAnswer { invocationOnMock ->
-            (invocationOnMock.arguments[0] as RemoteDataSource.LoadTVShowsCallback).onAllTVShowReceived(tvShowResponses)
+            (invocationOnMock.arguments[0] as RemoteDataSource.LoadTVShowsCallback).onAllTVShowReceived(
+                tvShowResponses
+            )
         }.`when`(remote).fetchTVShowData(any())
         val tvShowData = LiveDataTestUtil.getValue(fakeMovieRepository.fetchTVShowData())
         verify(remote).fetchTVShowData(any())
@@ -57,13 +61,16 @@ class MovieRepositoryTest {
     }
 
     @Test
-    fun testFetchMovieDataDetail(){
+    fun testFetchMovieDataDetail() {
         doAnswer { invocationOnMock ->
-            (invocationOnMock.arguments[1] as RemoteDataSource.LoadMovieDetailCallback).onMovieDetailReceived(detailMovie)
+            (invocationOnMock.arguments[1] as RemoteDataSource.LoadMovieDetailCallback).onMovieDetailReceived(
+                detailMovie
+            )
             null
         }.`when`(remote).fetchMovieDataDetail(eq(movieId), any())
 
-        val movieDetail = LiveDataTestUtil.getValue(fakeMovieRepository.fetchMovieDataDetail(movieId))
+        val movieDetail =
+            LiveDataTestUtil.getValue(fakeMovieRepository.fetchMovieDataDetail(movieId))
 
         verify(remote).fetchMovieDataDetail(eq(movieId), any())
 
@@ -74,36 +81,76 @@ class MovieRepositoryTest {
     @Test
     fun testFetchTvShowDataDetail() {
         doAnswer { invocationOnMock ->
-            (invocationOnMock.arguments[1] as RemoteDataSource.LoadTVShowDetailCallback).onTVShowDetailReceived(detailTVShow)
+            (invocationOnMock.arguments[1] as RemoteDataSource.LoadTVShowDetailCallback).onTVShowDetailReceived(
+                detailTVShow
+            )
             null
         }.`when`(remote).fetchTvShowDataDetail(eq(tvShowId), any())
-        val tvShowData = LiveDataTestUtil.getValue(fakeMovieRepository.fetchTvShowDataDetail(tvShowId))
+        val tvShowData =
+            LiveDataTestUtil.getValue(fakeMovieRepository.fetchTvShowDataDetail(tvShowId))
         verify(remote).fetchTvShowDataDetail(eq(tvShowId), any())
         assertNotNull(tvShowData)
         assertEquals(tvShowData, detailTVShow)
     }
 
     @Test
-    fun testMovieDetailCredits(){
+    fun testMovieDetailCredits() {
         doAnswer { invocationOnMock ->
-            (invocationOnMock.arguments[1] as RemoteDataSource.LoadMovieDetailCreditCallback).onMovieDetailCreditReceived(movieCredits)
+            (invocationOnMock.arguments[1] as RemoteDataSource.LoadMovieDetailCreditCallback).onMovieDetailCreditReceived(
+                movieCredits
+            )
             null
         }.`when`(remote).fetchMovieDetailCredits(eq(tvShowId), any())
-        val movieCreditsResponse = LiveDataTestUtil.getValue(fakeMovieRepository.fetchMovieDetailCredits(tvShowId))
+        val movieCreditsResponse =
+            LiveDataTestUtil.getValue(fakeMovieRepository.fetchMovieDetailCredits(tvShowId))
         verify(remote).fetchMovieDetailCredits(eq(tvShowId), any())
         assertNotNull(movieCreditsResponse)
         assertEquals(movieCreditsResponse, movieCredits)
     }
 
     @Test
-    fun testTVShowDetailCredits(){
+    fun testTVShowDetailCredits() {
         doAnswer { invocationOnMock ->
-            (invocationOnMock.arguments[1] as RemoteDataSource.LoadTVShowDetailCreditCallback).onTVShowDetailCreditReceived(tvShowCredits)
+            (invocationOnMock.arguments[1] as RemoteDataSource.LoadTVShowDetailCreditCallback).onTVShowDetailCreditReceived(
+                tvShowCredits
+            )
             null
         }.`when`(remote).fetchTVShowDetailCredits(eq(tvShowId), any())
-        val tvShowCreditsResponse = LiveDataTestUtil.getValue(fakeMovieRepository.fetchTVShowDetailCredits(tvShowId))
+        val tvShowCreditsResponse =
+            LiveDataTestUtil.getValue(fakeMovieRepository.fetchTVShowDetailCredits(tvShowId))
         verify(remote).fetchTVShowDetailCredits(eq(tvShowId), any())
         assertNotNull(tvShowCreditsResponse)
         assertEquals(tvShowCreditsResponse, tvShowCredits)
     }
+
+
+    @Test
+    fun searchMovies() {
+        doAnswer { invocationOnMock ->
+            (invocationOnMock.arguments[1] as RemoteDataSource.LoadMovieSearchCallback).onMovieFound(
+                movieResponses
+            )
+            null
+        }.`when`(remote).searchMovies(eq(movieSearchQuery), any())
+        val movies = LiveDataTestUtil.getValue(fakeMovieRepository.searchMovies(movieSearchQuery))
+        verify(remote).searchMovies(eq(movieSearchQuery), any())
+        assertNotNull(movies)
+        assertEquals(movies.size.toLong(), movieResponses.size.toLong())
+    }
+
+    @Test
+    fun searchTVShow() {
+        doAnswer { invocationOnMock ->
+            (invocationOnMock.arguments[1] as RemoteDataSource.LoadTVShowSearchCallback).onTVShowFound(
+                tvShowResponses
+            )
+            null
+        }.`when`(remote).searchTVShows(eq(tvShowSearchQuery), any())
+        val tvShowData =
+            LiveDataTestUtil.getValue(fakeMovieRepository.searchTVShows(tvShowSearchQuery))
+        verify(remote).searchTVShows(eq(tvShowSearchQuery), any())
+        assertNotNull(tvShowData)
+        assertEquals(tvShowData.size.toLong(), tvShowResponses.size.toLong())
+    }
+
 }
